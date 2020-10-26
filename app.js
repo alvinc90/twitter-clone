81d52808fc113ef6f415a12585b6c6b8971b04ff
alvinC <lvinz0790@gmail.com>
2020-10-25 17:26:13 -0700

    user auth works

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
+const bodyParser = require("body-parser"); 
 const mongoose = require('mongoose'); 
-
-const express = require("express"); 
-const app = express(); 
-
 const db = require('./config/keys').mongoURI; 
+const express = require("express"); 
+const users = require("./routes/api/users"); 
+const tweets = require("./routes/api/tweets");
+const passport = require('passport');
+
+const app = express(); 
+const port = process.env.PORT || 5000;
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello WandaVision"));
+app.get("/", (req, res) => {
+    // console.log(res) 
+    // debugger 
+    res.send("Hello San Francisco, I miss Marvel Movies")
+});
 
-const port = process.env.PORT || 5000;
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use("/api/users/", users); 
+app.use("/api/tweets", tweets); 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
